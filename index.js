50348241f0dc0bae4a548b83983dafac68061828
Rome Ojuro Otieno <otienorome2@gmail.com>
2024-10-02 09:04:57 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value'; // This will cause an error
+}
+
+
+
+
+
 // Write your solution in this file!
